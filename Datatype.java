d1b9bd6ee5fc5b2e5576839dbe1956a1533740e9
nikhilranjan01 <nikhilranjan2004@gmail.com>
2024-01-12 14:27:12 +0530

    updated

---
+++ Datatype.java
@@ -0,0 +1,18 @@
+public class Datatype {
+    public static void main(String[] args) {
+        // Different types of data
+        int intValue = 42;
+        double doubleValue = 3.14;
+        char charValue = 'A';
+        String stringValue = "Hello, Java!";
+        boolean booleanValue = true;
+
+        // Printing the values
+        System.out.println("Integer value: " + intValue);
+        System.out.println("Double value: " + doubleValue);
+        System.out.println("Character value: " + charValue);
+        System.out.println("String value: " + stringValue);
+        System.out.println("Boolean value: " + booleanValue);
+    }
+}
+
