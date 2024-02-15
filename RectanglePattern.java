bd32c15d1d314733c87dc1269d4f3182f93715aa
nikhilranjan01 <nikhilranjan2004@gmail.com>
2024-02-15 23:58:03 +0530

    updated

---
+++ RectanglePattern.java
@@ -0,0 +1,16 @@
+public class RectanglePattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        int columns = 7;
+        for(int i = 1; i <= rows; i++) {
+            for(int j = 1; j <= columns; j++) {
+                if(i == 1 || i == rows || j == 1 || j == columns) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
