38650f4e20029599a0f429805dc22ca349fb37aa
NIKHIL RANJAN⚡ <129507180+nikhilranjan01@users.noreply.github.com>
2024-08-30 23:50:47 +0530

    Update ArrayExample.java

--- ArrayExample.java
+++ ArrayExample.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class ArrayExample {
     public static void main(String[] args) {
-        int[] numbers = {5, 2, 8, 1, 6, 3}
+        int[] numbers = {5, 2, 8, 1, 6, 3};
 
         // Sorting the array
         Arrays.sort(numbers);
